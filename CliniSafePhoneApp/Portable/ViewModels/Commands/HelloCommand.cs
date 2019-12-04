b11ec93b044d06567f67fed9038dce77f866514c
Anthony Osei-Agyemang <anthony.osei-agyemang@clinisafe.com>
2019-12-04 10:52:53 +0000

    Updated Hello Command class file

--- CliniSafePhoneApp/Portable/ViewModels/Commands/HelloCommand.cs
+++ CliniSafePhoneApp/Portable/ViewModels/Commands/HelloCommand.cs
@@ -5,25 +5,41 @@ namespace CliniSafePhoneApp.Portable.ViewModels.Commands
 {
     public class HelloCommand : ICommand
     {
-        public TempTestViewModel _tempTestViewModels { get; set; }
+        /// <summary>
+        /// Declare a public property for TempTestViewModel
+        /// </summary>
+        public TempTestViewModel TempTestViewModel { get; set; }
 
+        /// <summary>
+        /// Initialise properties in constructor.
+        /// </summary>
+        /// <param name="tempTestViewModel"></param>
         public HelloCommand(TempTestViewModel tempTestViewModel)
         {
-            _tempTestViewModels = tempTestViewModel;
+            TempTestViewModel = tempTestViewModel;
         }
 
         public event EventHandler CanExecuteChanged;
 
-
+        /// <summary>
+        /// Enable Hello Command Button
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
         public bool CanExecute(object parameter)
         {
             return true;
         }
 
+        /// <summary>
+        /// Execute Hello Command
+        /// </summary>
+        /// <param name="parameter"></param>
         public void Execute(object parameter)
         {
-            _tempTestViewModels.HelloAsync();
-
+            TempTestViewModel.HelloAsync();
         }
     }
 }
+
+
