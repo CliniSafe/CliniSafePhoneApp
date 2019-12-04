91c4dc22d03aba4e0fcde43ffd897bbc0c0571f7
Anthony Osei-Agyemang <anthony.osei-agyemang@clinisafe.com>
2019-12-04 14:45:04 +0000

    Updated Find Drugs Page - In Progress

--- CliniSafePhoneApp/Portable/ViewModels/Commands/NavigateBackwardCommand.cs
+++ CliniSafePhoneApp/Portable/ViewModels/Commands/NavigateBackwardCommand.cs
@@ -5,26 +5,42 @@ namespace CliniSafePhoneApp.Portable.ViewModels.Commands
 {
     public class NavigateBackwardCommand : ICommand
     {
-        public NoConnectionViewModel _noConnectionViewModel { get; set; }
+        /// <summary>
+        /// Declare a public property for NoConnectionViewModel
+        /// </summary>
+        public NoConnectionViewModel NoConnectionViewModel { get; set; }
 
+
+        /// <summary>
+        /// Initialise properties in constructor.
+        /// </summary>
+        /// <param name="noConnectionViewModel"></param>
         public NavigateBackwardCommand(NoConnectionViewModel noConnectionViewModel)
         {
-            _noConnectionViewModel = noConnectionViewModel;
+            NoConnectionViewModel = noConnectionViewModel;
         }
 
         public event EventHandler CanExecuteChanged;
-
-
+        
+        /// <summary>
+        /// Enable Navigate Backward Command Button
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
         public bool CanExecute(object parameter)
         {
             return true;
         }
 
+        /// <summary>
+        /// Execute Navigate Backward Command
+        /// </summary>
+        /// <param name="parameter"></param>
         public void Execute(object parameter)
         {
             // _noConnectionViewModel.NavigateToHomePage();
 
-            _noConnectionViewModel.NavigateBack();
+            NoConnectionViewModel.NavigateBack();
         }
     }
 }
