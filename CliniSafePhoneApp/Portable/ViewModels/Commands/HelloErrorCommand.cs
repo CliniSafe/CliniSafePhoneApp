cb9de46b65cf512339a6a2348d5588bd4d1e30cd
Anthony Osei-Agyemang <anthony.osei-agyemang@clinisafe.com>
2019-12-04 12:00:33 +0000

    Updated Hello Error Command Class

--- CliniSafePhoneApp/Portable/ViewModels/Commands/HelloErrorCommand.cs
+++ CliniSafePhoneApp/Portable/ViewModels/Commands/HelloErrorCommand.cs
@@ -5,25 +5,39 @@ namespace CliniSafePhoneApp.Portable.ViewModels.Commands
 {
     public class HelloErrorCommand : ICommand
     {
-        public TempTestViewModel _tempTestViewModels { get; set; }
+        /// <summary>
+        /// Declare a public property for TempTestViewModel
+        /// </summary>
+        public TempTestViewModel TempTestViewModels { get; set; }
 
+        /// <summary>
+        /// Initialise properties in constructor.
+        /// </summary>
+        /// <param name="tempTestViewModel"></param>
         public HelloErrorCommand(TempTestViewModel tempTestViewModel)
         {
-            _tempTestViewModels = tempTestViewModel;
+            TempTestViewModels = tempTestViewModel;
         }
 
         public event EventHandler CanExecuteChanged;
 
-
+        /// <summary>
+        /// Enable Hello Command Button
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
         public bool CanExecute(object parameter)
         {
             return true;
         }
 
+        /// <summary>
+        /// Execute Hello Command Command
+        /// </summary>
+        /// <param name="parameter"></param>
         public void Execute(object parameter)
         {
-            _tempTestViewModels.HelloErrorAsync();
-
+            TempTestViewModels.HelloErrorAsync();
         }
     }
 }
